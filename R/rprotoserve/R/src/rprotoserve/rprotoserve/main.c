14fe9e0d25e8e6f538b88669a4899afff149d71f
Chris Hinshaw <chris.hinshaw@gmail.com>
2013-12-16 20:38:10 -0600

    Initial Commit

---
+++ R/rprotoserve/R/src/rprotoserve/rprotoserve/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  rprotoserve
+//
+//  Created by Chris Hinshaw on 12/16/13.
+//  Copyright (c) 2013 Chris Hinshaw. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
