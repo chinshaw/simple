0f1e74387b9c9e2bc5d6be0e6f9fa0dac117722b
Chris Hinshaw <chris.hinshaw@gmail.com>
2014-05-17 07:35:05 -0500

    Adding the new site configuration code.

---
+++ modules/orchestrator/server/src/main/java/com/simple/orchestrator/hadoop/config/SiteConfigurationException.java
@@ -0,0 +1,29 @@
+package com.simple.orchestrator.hadoop.config;
+
+public class SiteConfigurationException extends Exception {
+
+	public SiteConfigurationException() {
+		
+	}
+
+	public SiteConfigurationException(String message) {
+		super(message);
+		
+	}
+
+	public SiteConfigurationException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	public SiteConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public SiteConfigurationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+}
