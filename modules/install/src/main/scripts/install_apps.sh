4725860f13410683d31592345954f92cfdbce18d
Chris Hinshaw <chris.hinshaw@gmail.com>
2014-05-22 08:10:30 -0500

    Committing updates to the install apps shell script.

--- modules/install/src/main/scripts/install_apps.sh
+++ modules/install/src/main/scripts/install_apps.sh
@@ -16,34 +16,40 @@ TOMCAT_VERSIONS_DIR=${VERSIONS_DIR}/${TOMCAT_VERSION}
 R_VERSIONS_DIR=${VERSIONS_DIR}/${R_VERSION}
 ZOO_VERSIONS_DIR=${VERSIONS_DIR}/${ZOO_VERSION}
 
-HADOOP_URL=http://apache.mirrors.tds.net/hadoop/core/${HADOOP_VERSION}/${HADOOP_VERSION}.tar.gz
-HBASE_URL=http://apache.mirrors.tds.net/hbase/${HBASE_VERSION}/${HBASE_VERSION}-${HBASE_HADOOP_VERSION}-bin.tar.gz
+#HADOOP_URL=http://apache.mirrors.tds.net/hadoop/core/${HADOOP_VERSION}/${HADOOP_VERSION}.tar.gz
+#HBASE_URL=http://apache.mirrors.tds.net/hbase/${HBASE_VERSION}/${HBASE_VERSION}-${HBASE_HADOOP_VERSION}-bin.tar.gz
 # Note this url will not work in the future if the base version changes.
-TOMCAT_URL=http://www.dsgnwrld.com/am/tomcat/tomcat-7/v7.0.53/bin/${TOMCAT_VERSION}.tar.gz
-R_URL=http://cran.revolutionanalytics.com/src/base/R-3/${R_VERSION}.tar.gz
-ZOO_URL=http://www.trieuvan.com/apache/zookeeper/${ZOO_VERSION}/${ZOO_VERSION}.tar.gz
+#TOMCAT_URL=http://www.dsgnwrld.com/am/tomcat/tomcat-7/v7.0.53/bin/${TOMCAT_VERSION}.tar.gz
+#R_URL=http://cran.revolutionanalytics.com/src/base/R-3/${R_VERSION}.tar.gz
+#ZOO_URL=http://www.trieuvan.com/apache/zookeeper/${ZOO_VERSION}/${ZOO_VERSION}.tar.gz
+
+HADOOP_URL=file://$(pwd)/../resources/${HADOOP_VERSION}.tar.gz
+HBASE_URL=file://$(pwd)/../resources/${HBASE_VERSION}-${HBASE_HADOOP_VERSION}-bin.tar.gz
+TOMCAT_URL=file://$(pwd)/../resources/${TOMCAT_VERSION}.tar.gz
+R_URL=file://$(pwd)/../resources/${R_VERSION}.tar.gz
+ZOO_URL=file://$(pwd)/../resources/${ZOO_VERSION}.tar.gz
 
 
 R_CONFIG_OPTIONS="--without-x --with-cairo --with-libpng"
 
 do_install_hadoop() {
 	if [[ ! -d ${HADOOP_VERSIONS_DIR} ]]; then
-		cd ${VERSION_DIR} && curl ${HADOOP_URL} | tar xz
-		ln -s ${HADOOP_VERSION} ../hadoop
+		cd ${VERSIONS_DIR} && curl ${HADOOP_URL} | tar xz
+		cd ${APPS_DIR} && ln -sf  versions/${HADOOP_VERSION} hadoop
 	fi;
 }
 
 do_install_hbase() {
 	if [[ ! -d ${HBASE_VERSIONS_DIR} ]]; then
 		cd ${VERSIONS_DIR} && curl ${HBASE_URL} | tar xz
-		ln -s ${HBASE_VERSION} ../hbase
+		cd ${APPS_DIR} && ln -sf versions/${HBASE_VERSION}-${HBASE_HADOOP_VERSION} hbase
 	fi;
 }
 
 do_install_tomcat() {
 	if [[ ! -d ${TOMCAT_VERSIONS_DIR} ]]; then 
 		cd ${VERSIONS_DIR} && curl ${TOMCAT_URL} | tar xz
-		ln -s ${TOMCAT_VERSION} ../tomcat
+		cd ${APPS_DIR} && ln -sf versions/${TOMCAT_VERSION} tomcat
 	fi;
 }
 
@@ -53,7 +59,7 @@ do_install_r() {
 		cd ${r_install_base} && curl ${R_URL} | tar xz
 		cd ${r_install_base}/${R_VERSION}
 		./configure --prefix=${R_VERSIONS_DIR} ${R_CONFIG_OPTIONS} && make && make install
-		cd ${VERSIONS_DIR} && ln -s ${R_VERSION} ../R
+		cd ${APPS_DIR} && ln -sf versions/${R_VERSION} R
 	fi;
 }
 
@@ -69,12 +75,10 @@ do_install_zoo() {
 install() {
 	mkdir -p ${VERSIONS_DIR}
 	do_install_zoo;
-	#do_install_hadoop;
-	#do_install_hbase;
-	#do_install_tomcat;
-	#do_install_r;
-	
+	do_install_hadoop;
+	do_install_hbase;
+	do_install_tomcat;
+	do_install_r;
 }
 
 install;
-
