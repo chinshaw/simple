4cdedc2fbcb80c02ee7bee1d6cc06dee92ce52cd
Chris Hinshaw <chris.hinshaw@gmail.com>
2014-05-29 17:23:11 -0500

    Orchestrator
    Working on the metric service.

---
+++ modules/orchestrator/client/src/main/java/com/artisan/orchestrator/rest/client/MetricService.java
@@ -0,0 +1,55 @@
+package com.artisan.orchestrator.rest.client;
+
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+
+import com.simple.api.orchestrator.IMetric;
+import com.simple.orchestrator.api.service.IMetricService;
+import com.sun.jersey.api.client.WebResource;
+
+public class MetricService implements IMetricService {
+
+	public static final String SERVICE_PATH = "metric";
+
+	private final WebResource resource;
+
+	public MetricService(ArtisanClient client, String baseUrl) {
+		resource = client.resource(baseUrl + "/" + SERVICE_PATH);
+		resource.accept(MediaType.APPLICATION_JSON);
+	}
+
+	/**
+	 * 
+	 * 
+	 * 
+	 * @GET
+	 * @Path("/{rowKey ")
+	 * @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON,
+	 *             MediaType.APPLICATION_PROTOBUF })
+	 */
+	@Override
+	public IMetric find(String rowKey) {
+		IMetric metric = resource.path(rowKey).type(MediaType.APPLICATION_JSON).get(IMetric.class);
+		return metric;
+	}
+
+	/**
+	 * Implementation for fetching a specify value by it's row key column and qualifier
+	 * 
+	 * @GET
+	 * @Path("/{rowKey /{column}:{qualifier}")
+	 * @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON,
+	 *             MediaType.APPLICATION_PROTOBUF })
+	 */
+	@Override
+	public IMetric find(String rowKey, String column, String qualifier) {
+		return resource.path(rowKey).path(column + ":" + qualifier).get(IMetric.class);
+	}
+
+	@Override
+	public List<IMetric> find(Long operationId) {
+		List<IMetric> metrics = resource.path("operation").path(operationId.toString()).accept(MediaType.APPLICATION_JSON).get(List.class);
+		return metrics;
+	}
+}
--- modules/orchestrator/server/src/main/java/com/simple/orchestrator/service/web/rest/OperationExecutionResource.java
+++ modules/orchestrator/server/src/main/java/com/simple/orchestrator/service/web/rest/OperationExecutionResource.java
@@ -12,10 +12,10 @@ import com.simple.domain.model.dataprovider.DataProvider;
 import com.simple.domain.model.ui.AnalyticsOperationInput;
 import com.simple.orchestrator.api.IHadoopOperationJobConfiguration;
 import com.simple.orchestrator.api.IJobProgress;
-import com.simple.orchestrator.api.IOperationExecutionService;
 import com.simple.orchestrator.api.exception.HadoopJobException;
 import com.simple.orchestrator.api.exception.InvalidJobIdException;
 import com.simple.orchestrator.api.rest.MediaType;
+import com.simple.orchestrator.api.service.IOperationExecutionService;
 import com.simple.orchestrator.hadoop.mrv2.OperationDriver;
 
 @Path("/operation")
