abb59ab32ca91d58aa9cc0ad873338e92a9258ec
Chris Hinshaw <chris.hinshaw@gmail.com>
2014-10-30 19:16:50 -0500

    Adding taske execution file

---
+++ modules/api/target/artisan-api-1.1-SNAPSHOT/WEB-INF/classes/com/simple/api/orchestrator/ITaskExecution.java
@@ -0,0 +1,35 @@
+package com.simple.api.orchestrator;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+public interface ITaskExecution extends Serializable {
+
+	public enum TaskCompletionStatus {
+	    SUCCESS, FAILED, VIOLATIONS
+	}
+
+	
+    /**
+     * Getter for the log output.
+     * 
+     * @return
+     */
+    public abstract String getExecutionLog();
+
+    public abstract Date getStartTime();
+
+    public abstract Date getCompletionTime();
+    
+    public abstract TaskCompletionStatus getCompletionStatus();
+
+    public abstract void setCompletionStatus(TaskCompletionStatus completionStatus);
+
+    public abstract List<? extends IMetric> getExecutionMetrics();
+
+	public abstract void createFailure(Throwable cause);
+
+	public abstract Long getId();
+}
